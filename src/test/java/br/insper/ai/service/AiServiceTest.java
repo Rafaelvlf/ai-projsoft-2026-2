e51ed5d5bb1d7fa8087807dd9a01b7fcf6030200
Rafaelvlf <rafaelvlf1@al.insper.edu.br>
2026-09-22 21:52:12 -0300

    Feat: action de deploy automático e teste para pull request na main

--- src/test/java/br/insper/ai/service/AiServiceTest.java
+++ src/test/java/br/insper/ai/service/AiServiceTest.java
@@ -6,7 +6,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
-public class PagamentoServiceTest {
+public class AiServiceTest {
 
     @InjectMocks
     private AiService aiService;
