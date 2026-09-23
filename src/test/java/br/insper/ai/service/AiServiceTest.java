2ff612404bc511971a2829cf8d0dd55088cec6a1
Rafaelvlf <rafaelvlf1@al.insper.edu.br>
2026-09-22 21:50:27 -0300

    Fix: para de rastrear application.properties

---
+++ src/test/java/br/insper/ai/service/AiServiceTest.java
@@ -0,0 +1,18 @@
+package br.insper.ai.service;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class PagamentoServiceTest {
+
+    @InjectMocks
+    private AiService aiService;
+
+    @Mock
+    private AiRepository aiRepository;
+
+}	
+	
